4b4483fe3a9fd96162f11a1b79935c0f597b2474
HiGuy Smith <higuymb@gmail.com>
2017-08-14 23:35:50 -0400

    WS server basics

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 var Discord = require("discord.io");
+var WebSocket = require("ws");
+
 var bot = new Discord.Client({
 	token: process.argv[2],
 	autorun: true
 });
 
+var wss = new WebSocket.Server({
+	port: 28069
+});
+
 bot.on('ready', function() {
 	console.log('Ready! %s - %s', bot.username, bot.id);
+});
+
+wss.on('connection', function(ws) {
+	ws.on('message', function(message) {
+
+	});
 });
