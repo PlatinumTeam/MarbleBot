34dfd9bc387f0d56aa95ed5f7cac5cd9fda88682
Jeff Hutchinson <programmerjeff95@gmail.com>
2017-08-16 23:13:55 -0400

    better error checking in building the command dictionary

--- chat/MessageHandler.js
+++ chat/MessageHandler.js
@@ -11,9 +11,12 @@ module.exports = class MessageHandler extends EventEmitter {
 		this.server = server;
 
 		for (let event in commandDictionary) {
-			// Make sure handle is a function.
+			// Make sure handle and parse are functions.
 			if (commandDictionary[event].handle === undefined) {
-				throw new Error("Event " + event + " class does not have a static 'handle' definition.");
+				throw new Error("Event " + event + " does not have a 'handle' function definition.");
+			}
+			if (commandDictionary[event].parse === undefined) {
+				throw new Error("Event " + event + " does not have a 'parse' function definition.");
 			}
 
 			this.on(event, (client, words, data) => {
