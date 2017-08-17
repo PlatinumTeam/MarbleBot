1a4668336b32b4ba31ebe3b6cf7a961dde65d8a2
HiGuy Smith <higuymb@gmail.com>
2017-08-16 21:07:17 -0400

    Basic chatting

--- chat/Client.js
+++ chat/Client.js
@@ -1,4 +1,5 @@
 const EventEmitter = require('events');
+const util = require('util');
 
 module.exports = class Client extends EventEmitter {
 	get username() {
@@ -18,6 +19,15 @@ module.exports = class Client extends EventEmitter {
 
 		this.on('logged', () => {
 			this._sendRaw('LOGGED');
+		});
+		this.on('chat', (info) => {
+			this._sendRaw(util.format('CHAT %s %s %s %s %s',
+				info.sender.username, //username
+				info.sender.username, //display
+				info.destination,
+				0, //access
+				info.message
+			));
 		});
 	}
 
